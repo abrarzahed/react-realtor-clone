522094c3c2497d828647c6e630a67f0cdf08a495
abrarzahed <abrarzahed6986@gmail.com>
2022-10-22 23:43:10 +0600

    completed createlisting form handling

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function CreateListing() {
   const [formData, setFormData] = useState({
-    type: "rent",
+    type: "sell",
     name: "",
     bedrooms: 2,
     bathrooms: 1,
@@ -13,9 +13,42 @@ export default function CreateListing() {
     offer: true,
     regularPrice: "",
     discountPrice: "",
+    images: {},
   });
 
-  const onFieldsChange = () => {};
+  const onFieldsChange = (e, fieldValue) => {
+    e.preventDefault();
+    const { name, type, value, files } = e.target;
+
+    console.log(files);
+
+    if (type === "button") {
+      setFormData((prev) => {
+        return {
+          ...prev,
+          [name]: fieldValue,
+        };
+      });
+    }
+
+    if (type === "text" || type === "textarea" || type === "number") {
+      setFormData((prev) => {
+        return {
+          ...prev,
+          [name]: value,
+        };
+      });
+    }
+
+    if (type === "file") {
+      setFormData((prev) => {
+        return {
+          ...prev,
+          [name]: files,
+        };
+      });
+    }
+  };
 
   return (
     <main className="px-4 my-8">
@@ -23,7 +56,7 @@ export default function CreateListing() {
         <h1 className="text-3xl border-b-2 pb-[14px] mb-6 font-bold text-gray-800">
           Create a Listing
         </h1>
-        <form id="form">
+        <form>
           {/* ============ Sell or Rent ============ */}
           <div className="my-8 input-group">
             <p className="font-semibold mb-3 text-lg text-gray-700">
@@ -31,23 +64,25 @@ export default function CreateListing() {
             </p>
             <div className="flex space-x-8 items-center">
               <button
-                name="sell"
+                onClick={(e) => onFieldsChange(e, "sell")}
+                name="type"
                 type="button"
-                className={`w-full border  border-blue-300 rounded p-3 text-xl font-semibold ${
+                className={`w-full border  border-cyan-300 rounded p-3 text-xl font-semibold ${
                   formData.type === "sell"
-                    ? "bg-blue-900 text-white"
-                    : "bg-white text-blue-600"
+                    ? "bg-cyan-900 text-white"
+                    : "bg-white text-cyan-600"
                 }`}
               >
                 Sell
               </button>
               <button
-                name="rent"
+                onClick={(e) => onFieldsChange(e, "rent")}
+                name="type"
                 type="button"
-                className={`w-full border border-blue-300  rounded p-3 text-xl font-semibold ${
+                className={`w-full border border-cyan-300  rounded p-3 text-xl font-semibold ${
                   formData.type === "rent"
-                    ? "bg-blue-900 text-white"
-                    : "bg-white text-blue-600"
+                    ? "bg-cyan-900 text-white"
+                    : "bg-white text-cyan-600"
                 }`}
               >
                 Rent
@@ -65,7 +100,7 @@ export default function CreateListing() {
               maxLength="32"
               type="text"
               placeholder="Name"
-              className="w-full rounded py-4 border border-blue-400 font-semibold"
+              className="w-full rounded py-4 border border-cyan-400 focus:border-cyan-300 font-semibold"
               required
               onChange={onFieldsChange}
             />
@@ -82,7 +117,7 @@ export default function CreateListing() {
                 value={formData.bedrooms}
                 type="number"
                 placeholder="4"
-                className="w-full rounded py-[14px] border border-blue-400 text-center text-xl font-semibold"
+                className="w-full rounded py-[14px] border border-cyan-400 focus:border-cyan-300 text-center text-xl font-semibold"
                 onChange={onFieldsChange}
                 required
               />
@@ -91,7 +126,7 @@ export default function CreateListing() {
                 value={formData.bathrooms}
                 placeholder="2"
                 type="number"
-                className="w-full rounded py-[14px] border border-blue-400 text-center text-xl font-semibold"
+                className="w-full rounded py-[14px] border border-cyan-400 focus:border-cyan-300 text-center text-xl font-semibold"
                 onChange={onFieldsChange}
                 required
               />
@@ -107,22 +142,24 @@ export default function CreateListing() {
               <button
                 name="parkingSpot"
                 type="button"
-                className={`w-full border  border-blue-300 rounded p-3 text-xl font-semibold ${
+                className={`w-full border  border-cyan-300 rounded p-3 text-xl font-semibold ${
                   formData.parkingSpot
-                    ? "bg-blue-900 text-white"
-                    : "bg-white text-blue-600"
+                    ? "bg-cyan-900 text-white"
+                    : "bg-white text-cyan-600"
                 }`}
+                onClick={(e) => onFieldsChange(e, true)}
               >
                 Yes
               </button>
               <button
                 name="parkingSpot"
                 type="button"
-                className={`w-full border  border-blue-300 rounded p-3 text-xl font-semibold ${
+                className={`w-full border  border-cyan-300 rounded p-3 text-xl font-semibold ${
                   !formData.parkingSpot
-                    ? "bg-blue-900 text-white"
-                    : "bg-white text-blue-600"
+                    ? "bg-cyan-900 text-white"
+                    : "bg-white text-cyan-600"
                 }`}
+                onClick={(e) => onFieldsChange(e, false)}
               >
                 No
               </button>
@@ -138,22 +175,24 @@ export default function CreateListing() {
               <button
                 name="isFurnished"
                 type="button"
-                className={`w-full border  border-blue-300 rounded p-3 text-xl font-semibold ${
+                className={`w-full border  border-cyan-300 rounded p-3 text-xl font-semibold ${
                   formData.isFurnished
-                    ? "bg-blue-900 text-white"
-                    : "bg-white text-blue-600"
+                    ? "bg-cyan-900 text-white"
+                    : "bg-white text-cyan-600"
                 }`}
+                onClick={(e) => onFieldsChange(e, true)}
               >
                 Yes
               </button>
               <button
                 name="isFurnished"
                 type="button"
-                className={`w-full border  border-blue-300 rounded p-3 text-xl font-semibold ${
+                className={`w-full border  border-cyan-300 rounded p-3 text-xl font-semibold ${
                   !formData.isFurnished
-                    ? "bg-blue-900 text-white"
-                    : "bg-white text-blue-600"
+                    ? "bg-cyan-900 text-white"
+                    : "bg-white text-cyan-600"
                 }`}
+                onClick={(e) => onFieldsChange(e, false)}
               >
                 No
               </button>
@@ -168,7 +207,7 @@ export default function CreateListing() {
               value={formData.address}
               type="text"
               placeholder="Address"
-              className="w-full rounded py-3 border border-blue-400 font-semibold"
+              className="w-full rounded py-3 border border-cyan-400 focus:border-cyan-300 font-semibold"
               required
               onChange={onFieldsChange}
             ></textarea>
@@ -184,7 +223,7 @@ export default function CreateListing() {
               value={formData.description}
               type="text"
               placeholder="Description"
-              className="w-full rounded py-3 border border-blue-400 font-semibold"
+              className="w-full rounded py-3 border border-cyan-400 focus:border-cyan-300 font-semibold"
               required
               onChange={onFieldsChange}
             ></textarea>
@@ -197,22 +236,24 @@ export default function CreateListing() {
               <button
                 name="offer"
                 type="button"
-                className={`w-full border  border-blue-300 rounded p-3 text-xl font-semibold ${
+                className={`w-full border  border-cyan-300 rounded p-3 text-xl font-semibold ${
                   formData.offer
-                    ? "bg-blue-900 text-white"
-                    : "bg-white text-blue-600"
+                    ? "bg-cyan-900 text-white"
+                    : "bg-white text-cyan-600"
                 }`}
+                onClick={(e) => onFieldsChange(e, true)}
               >
                 Yes
               </button>
               <button
                 name="offer"
                 type="button"
-                className={`w-full border  border-blue-300 rounded p-3 text-xl font-semibold ${
+                className={`w-full border  border-cyan-300 rounded p-3 text-xl font-semibold ${
                   !formData.offer
-                    ? "bg-blue-900 text-white"
-                    : "bg-white text-blue-600"
+                    ? "bg-cyan-900 text-white"
+                    : "bg-white text-cyan-600"
                 }`}
+                onClick={(e) => onFieldsChange(e, false)}
               >
                 No
               </button>
@@ -227,18 +268,20 @@ export default function CreateListing() {
             <div className="flex space-x-8 items-center">
               <input
                 name="regularPrice"
+                placeholder="1000"
                 value={formData.regularPrice}
                 type="number"
-                className="w-full rounded py-[14px] border border-blue-400 text-center text-xl font-semibold"
+                className="w-full rounded py-[14px] border border-cyan-400 focus:border-cyan-300 text-center text-xl font-semibold"
                 onChange={onFieldsChange}
                 required
               />
               <input
+                placeholder={formData.offer ? "120" : ""}
                 disabled={!formData.offer}
                 name="discountPrice"
                 value={formData.discountPrice}
                 type="number"
-                className="w-full rounded py-[14px] border border-blue-400 text-center text-xl font-semibold disabled:bg-gray-200 disabled:border-gray-200 disabled:cursor-none"
+                className="w-full rounded py-[14px] border border-cyan-400 focus:border-cyan-300 text-center text-xl font-semibold disabled:bg-gray-200 disabled:border-gray-200"
                 onChange={onFieldsChange}
                 required={formData.offer}
               />
@@ -254,21 +297,20 @@ export default function CreateListing() {
             <div className="flex space-x-8 items-center">
               <input
                 name="images"
-                value={formData.regularPrice}
                 type="file"
-                className="w-full rounded px-[14px] py-[14px] border border-blue-400 text-center font-semibold"
-                onChange={onFieldsChange}
+                className="w-full rounded px-[14px] py-[14px] border border-cyan-400 focus:border-cyan-300 text-center font-semibold"
+                onChange={(e) => onFieldsChange(e)}
                 accept=".jpg,.jpeg,.png,.svg"
                 required
                 multiple
-                max="6"
+                maxLength="6"
               />
             </div>
           </div>
 
           <button
             type="submit"
-            className="bg-red-500 w-full rounded text-white p-4 shadow-sm uppercase text-md font-medium hover:bg-red-700 transition duration-250 disabled:bg-gray-200 disabled:cursor-none"
+            className="bg-blue-900 w-full rounded text-white p-4 shadow-sm uppercase text-md font-medium hover:bg-blue-800 transition duration-250 disabled:bg-gray-200 disabled:cursor-none"
           >
             Create Listing
           </button>
